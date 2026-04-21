bb459835a2aa8b6ad8f1b3d59c3be8a5f198ca8a
Muhammad Akhza Fachrozy <135035381+akhzaozy@users.noreply.github.com>
2026-04-21 21:59:25 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aplikasi Kucingarong TERBANG</title>
+    <title>Aplikasi Kucingarong TERBANG JATUH LAGI KOCAKLAH</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         /* Gaya tambahan untuk tema gelap */
